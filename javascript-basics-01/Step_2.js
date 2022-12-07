7f94f4fc16c9022815a8659b52eb719b71343616
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2022-12-07 19:02:50 +0200

    completed step_2

---
+++ javascript-basics-01/Step_2.js
@@ -0,0 +1,11 @@
+// Define three variables: name, surname and city.
+
+let name = "Hisham" ;
+let surname = "Ahdab" ;
+let city = "Tripoli" ;
+
+
+
+// Show these variable in an alert box 
+
+window.alert(  "Name : " + name + "\n" +  "Surname : " + surname + "\n" + "City : " + city ) ;
