6a63e842fae92d969e17a94b7ac508293af1de32
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2022-12-08 12:08:23 +0200

    completed External files and Simple Math

--- javascript-basics-01/Step_6.js
+++ javascript-basics-01/Step_6.js
@@ -2,7 +2,7 @@ document.getElementById("validate").onclick = function() {
     let num1 = document.getElementById("first_number").value ;
     let num2 = document.getElementById("second_number").value ;
 
-    let mul = parseInt(num1) / parseInt(num2) ;
+    let mul = parseInt(num1) % parseInt(num2) ;
     prompt("do you want to make a divide  say | yes | or | y |  to go to divide ? ") 
-    alert("First number : " + num1 + "\nSecond number : " +  num2 + "\nThe multiplication : " + mul); 
+    alert("First number : " + num1 + "\nSecond number : " +  num2 + "\nThe The divistion : " + mul); 
  } ;
