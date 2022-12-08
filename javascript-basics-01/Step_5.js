33cd83cd60d1639765fa2df795fde1b40bc0e79a
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2022-12-08 11:09:59 +0200

    completed the step 5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,8 @@
+document.getElementById("validate").onclick = function() {
+    let num1 = document.getElementById("first_number").value ;
+    let num2 = document.getElementById("second_number").value ;
+
+    let mul = parseInt(num1) * parseInt(num2) ;
+    prompt("do you want to make a multiplication  say yes| or | y  to go to multiplication ? ") 
+    alert("First number : " + num1 + "\nSecond number : " +  num2 + "\nThe multiplication : " + mul); 
+ } ;
