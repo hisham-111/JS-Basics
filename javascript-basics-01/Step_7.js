a8888340a1241fa7282caa711aa799b9ddf63707
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2022-12-08 16:41:44 +0200

    completed Step 7 : Multiple operations

--- javascript-basics-01/Step_7.js
+++ javascript-basics-01/Step_7.js
@@ -3,5 +3,5 @@ document.getElementById("validate").onclick = function() {
     let num2 = document.getElementById("year").value ;
     let solution = (num1 * 2 + (5) ) *50 - num2  + (1766) ;
     prompt("let's make a small game  are you ready ? Say yes to continue  ") ;
-    alert(solution);
+    alert(solution) ;
 };
