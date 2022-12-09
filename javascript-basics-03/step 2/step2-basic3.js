074bccfa26a87801355742defc48616c08920286
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2022-12-09 14:47:29 +0200

    completed step1-basic3JS

---
+++ javascript-basics-03/step 2/step2-basic3.js
@@ -0,0 +1,11 @@
+let button = document.querySelectorAll('a')
+let paragraph = document.querySelectorAll('texte')
+button.forEach ((visibleHide) =>{ 
+visibleHide.addEventListener('click' , (visibleHide) => {
+  if(visibleHide.target.id== 'hide'){
+    texte.style = "display: none";
+  }
+  else {
+    texte.style = 'display:block';}
+  })
+})
