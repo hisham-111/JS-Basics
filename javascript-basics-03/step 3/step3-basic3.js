86592791d7869c3ed110dc7f08e125521982bf83
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2022-12-09 14:57:18 +0200

    completed step3 basic 3

---
+++ javascript-basics-03/step 3/step3-basic3.js
@@ -0,0 +1,14 @@
+let color1 = document.querySelector(".green");
+let color2 = document.querySelector(".red");
+let color3 = document.querySelector(".blue");
+color1.onclick = function(){
+    text.style.color = "green";
+}
+
+color2.onclick = function(){
+    text.style.color = "red";
+}
+
+color3.onclick = function(){
+    text.style.color = "blue";
+}
