40b368757b5b083f0d193ad2feb8e210127893d7
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2022-12-09 13:50:51 +0200

    completed step 3

--- javascript-basics-02/step 3/step3.js
+++ javascript-basics-02/step 3/step3.js
@@ -1,16 +1,10 @@
+const div = document.createElement("div")
+document.querySelector("body").appendChild(div).setAttribute("id", "other_div")
+const otherDiv = document.querySelector("#other_div")
 
-const div  = document.createElement("div");
-document.querySelector("body").appendChild(div).setAttribute("id" , "other_div");
-const other_div = document.querySelector("#other_div");
 
-const NameInput = document.querySelector("#name");
+const nameInput = document.querySelector("#name")
 
-NameInput.addEventListener("key" ,(e) =>
-{
-    other_div.innerText= NameInput.value;
+nameInput.addEventListener("keyup", (e)=> {
+    otherDiv.innerText = nameInput.value;
 })
-
-
-
-
-
